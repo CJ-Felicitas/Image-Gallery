1ab7e5874a2d11feb3191bfb6cb061e40c5fc9cc
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2022-01-25 11:39:48 +0800

    sql file

---
+++ imagegallery.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 25, 2022 at 04:39 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `imagegallery`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `imagegallery`
+--
+
+CREATE TABLE `imagegallery` (
+  `image_id` int(2) NOT NULL,
+  `dir` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `imagegallery`
+--
+
+INSERT INTO `imagegallery` (`image_id`, `dir`) VALUES
+(1, 'images/artsy_city.png'),
+(2, 'images/neon_city.png'),
+(3, 'images/sakura.png'),
+(4, 'images/cheems.jpg'),
+(5, 'images/5937023-middle.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `imagegallery`
+--
+ALTER TABLE `imagegallery`
+  ADD PRIMARY KEY (`image_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `imagegallery`
+--
+ALTER TABLE `imagegallery`
+  MODIFY `image_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
